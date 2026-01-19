c962f5309f0439dbd790f014e4dc46235fa22d3c
Steve Lam <stevyo99@yahoo.ca>
2026-01-19 14:50:48 -0800

    Add ValueType type and Type method to Value

--- js/value.go
+++ js/value.go
@@ -4,6 +4,36 @@ import (
 	"github.com/akshayganeshen/napi-go"
 )
 
+type ValueType napi.ValueType
+
+func (t ValueType) String() string {
+	switch napi.ValueType(t) {
+	case napi.ValueTypeUndefined:
+		return "undefined"
+	case napi.ValueTypeNull:
+		return "null"
+	case napi.ValueTypeBoolean:
+		return "boolean"
+	case napi.ValueTypeNumber:
+		return "number"
+	case napi.ValueTypeString:
+		return "string"
+	case napi.ValueTypeSymbol:
+		return "symbol"
+	case napi.ValueTypeObject:
+		return "object"
+	case napi.ValueTypeFunction:
+		return "function"
+	case napi.ValueTypeExternal:
+		return "external"
+	case napi.ValueTypeBigint:
+		return "bigint"
+
+	default:
+		return "other"
+	}
+}
+
 type Value struct {
 	Env   Env
 	Value napi.Value
@@ -11,4 +41,13 @@ type Value struct {
 
 func (v Value) GetEnv() Env {
 	return v.Env
+}
+
+func (v Value) Type() ValueType {
+	vt, st := napi.Typeof(v.Env.Env, v.Value)
+	if st != napi.StatusOK {
+		panic(napi.StatusError(st))
+	}
+
+	return ValueType(vt)
 }
