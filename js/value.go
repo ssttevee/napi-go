097bc1a1926ca402c1252040a677afb47e7af445
Steve Lam <stevyo99@yahoo.ca>
2026-01-19 18:13:04 -0800

    Move ValueType and its String method to value_type.go

--- js/value.go
+++ js/value.go
@@ -4,50 +4,27 @@ import (
 	"github.com/akshayganeshen/napi-go"
 )
 
-type ValueType napi.ValueType
-
-func (t ValueType) String() string {
-	switch napi.ValueType(t) {
-	case napi.ValueTypeUndefined:
-		return "undefined"
-	case napi.ValueTypeNull:
-		return "null"
-	case napi.ValueTypeBoolean:
-		return "boolean"
-	case napi.ValueTypeNumber:
-		return "number"
-	case napi.ValueTypeString:
-		return "string"
-	case napi.ValueTypeSymbol:
-		return "symbol"
-	case napi.ValueTypeObject:
-		return "object"
-	case napi.ValueTypeFunction:
-		return "function"
-	case napi.ValueTypeExternal:
-		return "external"
-	case napi.ValueTypeBigint:
-		return "bigint"
-
-	default:
-		return "other"
-	}
-}
-
 type Value struct {
 	Env   Env
 	Value napi.Value
+}
+
+func (e Env) AsValue(value napi.Value) Value {
+	return Value{
+		Env:   e,
+		Value: value,
+	}
 }
 
 func (v Value) GetEnv() Env {
 	return v.Env
 }
 
-func (v Value) Type() ValueType {
+func (v Value) Type() napi.ValueType {
 	vt, st := napi.Typeof(v.Env.Env, v.Value)
 	if st != napi.StatusOK {
 		panic(napi.StatusError(st))
 	}
 
-	return ValueType(vt)
+	return vt
 }
--- value_type.go
+++ value_type.go
@@ -19,3 +19,31 @@ const (
 	ValueTypeExternal  ValueType = C.napi_external
 	ValueTypeBigint    ValueType = C.napi_bigint
 )
+
+func (t ValueType) String() string {
+	switch ValueType(t) {
+	case ValueTypeUndefined:
+		return "undefined"
+	case ValueTypeNull:
+		return "null"
+	case ValueTypeBoolean:
+		return "boolean"
+	case ValueTypeNumber:
+		return "number"
+	case ValueTypeString:
+		return "string"
+	case ValueTypeSymbol:
+		return "symbol"
+	case ValueTypeObject:
+		return "object"
+	case ValueTypeFunction:
+		return "function"
+	case ValueTypeExternal:
+		return "external"
+	case ValueTypeBigint:
+		return "bigint"
+
+	default:
+		return "unknown"
+	}
+}
